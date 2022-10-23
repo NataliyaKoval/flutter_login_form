4ef2e4ac09393030602e3b24a1d4f6f9f66bdeb0
Nataly Koval <nataly.koval@n3wnormal.com>
2022-10-23 11:09:26 +0200

    Create Splash screen, add delay to it.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import 'screens/home_screen.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  initialize();
   runApp(const MyApp());
 }
+
+void initialize() async {
+  await Future.delayed(const Duration(seconds: 3));
+  FlutterNativeSplash.remove();
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
